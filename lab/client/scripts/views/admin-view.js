2c90e552944beca142e7fabaebd67270b588e7db
Allie <allie@codefellows.com>
2018-03-01 18:53:18 -0800

    adding lab 14

---
+++ lab/client/scripts/views/admin-view.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var app = app || {};
+
+(function (module) {
+  const adminView = {};
+
+  adminView.initAdminPage = function (ctx, next) {
+    $('.nav-menu').slideUp(350);
+    $('.admin-view').show();
+
+    $('#admin-form').on('submit', function(event) {
+      event.preventDefault();
+      let token = event.target.passphrase.value;
+
+      // COMMENT: Is the token cleared out of local storage? Do you agree or disagree with this structure?
+      $.get(`${__API_URL__}/api/v1/admin`, {token})
+        .then(res => {
+          localStorage.token = true;
+          page('/');
+        })
+        .catch(() => page('/'));
+    })
+  };
+
+  adminView.verify = function(ctx, next) {
+    if(!localStorage.token) $('.admin').addClass('admin-only');
+    else $('.admin').show();
+    next();
+  }
+
+  module.adminView = adminView;
+})(app)
