4eebd339938b612bfb1f85140945844907ab7d3f
Beverly Pham <beverly.pham@gmail.com>
2018-03-04 14:38:07 -0800

    answer comments

--- beverly/client/scripts/views/admin-view.js
+++ beverly/client/scripts/views/admin-view.js
@@ -14,6 +14,7 @@ var app = app || {};
       let token = event.target.passphrase.value;
 
       // COMMENT: Is the token cleared out of local storage? Do you agree or disagree with this structure?
+      // ANSWER: The token is not placed in local storage, just a value of "true" is placed in local storage. I agree with this structure. However, it looks like localStorage.token is given the value of "true" no matter if the response from the server is true or false, which it should only be given that value if the server responds with "true".
       $.get(`${__API_URL__}/api/v1/admin`, {token})
         .then(res => {
           localStorage.token = true;
